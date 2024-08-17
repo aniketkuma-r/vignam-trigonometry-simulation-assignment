c3a24820c23e1c67c0ec0910eed35f63e5cd084c
Aniket Kumar <aniketkumar8743130@gmail.com>
2024-08-17 12:06:15 +0530

    added typescript

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import TrigSimulation from './components/TrigSimulation.tsx';
+
+export default function App() {
+  document.title = "Trig Simulation";
+  return (
+    <TrigSimulation />
+  );
+};
